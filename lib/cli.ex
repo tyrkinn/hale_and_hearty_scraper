d34def1852051349c9289df829b1445d33e2d5aa
tyrkinn <nikemad61@gmail.com>
2022-06-14 20:11:30 +0300

    Initial commit

---
+++ lib/cli.ex
@@ -0,0 +1,50 @@
+defmodule CLI do
+  def main(argv) do
+    argv
+    |> parse_args()
+    |> process()
+  end
+
+  def parse_args(argv) do
+    args = OptionParser.parse(
+      argv,
+      strict: [help: :boolean, locations: :boolean],
+      alias: [h: :help]
+    )
+
+    case args do
+      {[help: true], _, _} ->
+        :help
+      {[], [], [{"-h", nil}]} ->
+        :help
+      {[locations: true], _, _} ->
+        :list_locations
+      {[], [], []} ->
+        :list_soups
+
+      _ ->
+        :invalid_arg
+    end
+  end
+
+  def process(:help) do
+    IO.puts """
+    soup --locations # Select a default location whose soups you want to list
+    soup # List the soups for a default location (you'll be prompted to select deafult locations if you haven't it already)
+    """
+    System.halt(0)
+  end
+
+  def process(:list_locations) do
+    Soup.enter_select_location_flow()
+  end
+
+  def process(:list_soups) do
+    Soup.fetch_soup_list()
+  end
+
+  def process(:invelid_arg) do
+    IO.puts "Invalid argument(s) passed. See usage below:"
+    process(:help)
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule Soup.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :soup,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      escript: [main_module: CLI],
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :httpoison]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, "~> 1.8.1"},
+      {:floki, "~> 0.11.0"}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
